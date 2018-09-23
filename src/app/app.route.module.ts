4fae201efdfd2ce8825164c5e097bbabd5da849e
Sathira <sathiraumesh@gmail.com>
2018-09-23 17:05:55 +0530

    created the authetication gaurds for the admin and the other users and added the authentication interceptor

--- src/app/app.route.module.ts
+++ src/app/app.route.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from "../../node_modules/@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./authetication/login/login.component";
 import { DrugInventoryModule } from "./drug-inventory/drug-inventory.module";
-import { UserAuthGaurd } from "./authetication/auth.gaurd";
+import { AdminAuthGaurd, DoctorAuthGaurd } from "./authetication/auth.gaurd";
+
 
 
 const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "/login" },
     { path: "login", component: LoginComponent },
-    { path: "drugs", loadChildren: "./drug-inventory/drug-inventory.module#DrugInventoryModule"},
-    { path: "admin", loadChildren: "./admin/admin.module#AdminModule"},
-    { path: "students", loadChildren: "./student/student.module#StudentModule"}
+    { path: "drugs", loadChildren: "./drug-inventory/drug-inventory.module#DrugInventoryModule",canActivate:[DoctorAuthGaurd]},
+    { path: "admin", loadChildren: "./admin/admin.module#AdminModule" ,canActivate:[AdminAuthGaurd]},
+    { path: "students", loadChildren: "./student/student.module#StudentModule"},
+    { path: "**",component:LoginComponent}
 
 ];
 
