6b955abacc00525a26701efe3f324f128fa1d3d8
RuchiraRuwantha <rruwantha222@gmail.com>
2018-09-25 22:15:39 +0530

    student module is updated

--- src/app/app.route.module.ts
+++ src/app/app.route.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./authetication/login/login.component";
 import { DrugInventoryModule } from "./drug-inventory/drug-inventory.module";
 import { UserAuthGaurd } from "./authetication/auth.gaurd";
+import { StudentModule } from "./student/student.module";
+import { AdminModule } from "./admin/admin.module";
 
 
 const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "/login" },
     { path: "login", component: LoginComponent },
-    { path: "drugs", loadChildren: "./drug-inventory/drug-inventory.module#DrugInventoryModule"},
-    { path: "admin", loadChildren: "./admin/admin.module#AdminModule"},
-    { path: "students", loadChildren: "./student/student.module#StudentModule"}
-
+    { path: "drugs", loadChildren: "./drug-inventory/drug-inventory.module#DrugInventoryModule" },
+    { path: "admin", loadChildren: "./admin/admin.module#AdminModule" },
+    { path: "students", loadChildren: "./student/student.module#StudentModule" }
 ];
 
 @NgModule({
